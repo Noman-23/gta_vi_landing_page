79a870c2fa67bfc135a72ea7f4355c0029decd57
Noman-AIG <noman23.aig@gmail.com>
2025-09-21 00:45:09 +0530

    Clean up the boilerplate, add the assets and installed the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main className='text-pink'>
+      <div>App</div>
+    </main>
+  );
+};
+export default App;
